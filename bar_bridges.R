4def6378d3d10f9226016c4caf443b02018652e2
jachan1 <jachan1@gmail.com>
2015-12-14 12:55:42 -0500

    sample script completed.

--- bar_bridges.R
+++ bar_bridges.R
@@ -1,18 +1,13 @@
 ## function
-plotfxn.bar<-function(ds, brks, cols,  min=0, max=100, title="", ylab="", xlab="", legtitle="", addblab=T, blabcol="black", base_font=12, fliplegend=F){
-    
-    
+plotfxn.bar <- function(ds, brks, cols,  min=0, max=100, title="", ylab="", xlab="", legtitle="", addblab=T, blabcol="black", base_font=12, fliplegend=F){
+    ## cols are added from IRCDA template colors
     rgb255 <- function(r,g,b){
         rgb(r/255, g/255, b/255)
     }
     if(missing(cols)){
         cols <- c(rgb255(166, 206, 227), rgb255(31, 120, 180), rgb255(178, 223, 138), rgb255(51, 160, 44), rgb255(251, 154, 153), rgb255(227, 26, 28), rgb255(253, 191, 111), rgb255(255, 127, 0), rgb255(202, 178, 214), rgb255(106, 61, 154))
-        
     }
-    
     ## need min and max
-    
-    
     grpm_fxn <- function(x) {
         x %>% mutate(semax=max(value) + 0.05*(max-min), 
                      pmax=max(value) + 0.055*(max-min), 
@@ -56,35 +51,3 @@ plotfxn.bar<-function(ds, brks, cols,  min=0, max=100, title="", ylab="", xlab="
     }
     g
 }
-
-
-## example data -- long
-ds1 <- data.frame(x_grp = sample(letters[1:3], 100, replace=T), leg_grp=sample(letters[1:2], 100, replace=T), 
-                  rvalue=runif(100, 0, 100))
-
-## example pre-processing -- means
-pds1 <- ds1 %>% group_by(x_grp, leg_grp) %>% 
-    summarise(value=mean(rvalue), plab="p-value=0.1", blab=sprintf("%1.0f%%", value))
-
-## example pre-processing -- props
-pds2 <- ds1 %>% group_by(x_grp, leg_grp) %>% 
-    summarise(value=100*sum(rvalue > 60)/n(), plab="p-value=0.1", blab=sprintf("%1.0f%%", value))
-
-## function call
-plotfxn.bar(pds1, title="Plot", ylab="Prop Over 60", xlab="X label", legtitle="Adherence", blabcol="white")
-plotfxn.bar(pds2, title="Plot", ylab="Mean Value", xlab="X label", legtitle="Adherence", blabcol="white")
-## requires a dataset with
-##      x_grp: factor that gives the groups on the x axis
-##      leg_grp: factor that gives the groups for the legend
-##      value: gives the height of each bar
-##      plab: label for the top of the bridge
-##      blab: label for the bar
-
-## brks control the breaks on the y axis. these may or may not be good
-## cols gives the colours for the bars. The order of the factor leg_grp will be the same as the order for the col
-## min and max give the limits for the y axis
-## ylab, xlab, title, and legtitle are titles for the graph
-## addblab controls whether or not the bars are labeled (this defaults to true)
-## blabcol will set the color of the bar label (default to black)
-
-## this returns a ggplot object so further manipulation with ggplot functions is possible after the function completes
